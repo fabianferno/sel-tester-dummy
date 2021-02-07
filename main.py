801926390e19f9f840995a307740cf9488b40b5d
Fabian Ferno <fabianferno@gmail.com>
2021-02-08 02:41:02 +0530

    Add driver

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome('./chromedriver')
-
-driver.get("https://www.python.org")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get("https://fabianferno.tech/")
+# elem = driver.find_element_by_name("q")
+# elem.send_keys("Username")
+# elem.send_keys(Keys.RETURN)
 
 print(driver.title)
